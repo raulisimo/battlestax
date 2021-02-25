c4b43e9f8d342aed4ee2169c0fca1e115f1c8c77
raulisimo <raulraul.sp@gmail.com>
2021-02-25 17:02:26 +0000

    Merging helloWorld into master

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -1,3 +1,8 @@
+// let's return a JSON response that looks like: { hello: "world" }
 exports.handler = async (event, context) => {
-  // let's return a JSON response that looks like: { hello: "world" }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ hello: "world" }),
+  };
 };
+
